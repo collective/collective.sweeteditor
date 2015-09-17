2da3c1e8847aad9824fc2dbdf77edb0710fb5cd5
Davide Moro <davide.moro@gmail,.com>
2015-09-17 18:19:40 +0200

    implement accordion behaviour

--- collective/sweeteditor/browser/js/sweeteditor.js
+++ collective/sweeteditor/browser/js/sweeteditor.js
@@ -1,16 +1,52 @@
 (function ($){
-    (function () {
-        // control expandable items
-        $(".collapsedHeading").live('click',function (){
-            var $heading = $(this);
-            $heading
-                .next()
-                .slideToggle(200, function (){
-                    $heading.toggleClass('collapsed');
+    $( document ).ready(function() {
+        (function () {
+            // control expandable items
+            $(".collapsedHeading").live('click', function (){
+                var $heading = $(this);
+                $heading
+                    .next()
+                    .slideToggle(200, function (){
+                        $heading.toggleClass('collapsed');
+                    });
+            });
+        }());
+        (function () {
+            // control accordion items
+            $(".accordionHeading").each(function () {
+                // init accordions
+                var $this = $(this), $prev, $next;
+                $prev = $this.prev('.accordionBody');
+                $next = $this.next('.accordionBody');
+
+                if ($prev.length === 0) {
+                    // this is the first accordion header
+                    $this.removeClass('collapsed');
+                    $this.addClass('first');
+                } else {
+                    $this.addClass('collapsed');
+                    $next.addClass('collapsed');
+                }
+
+                $nextNext = $next.next('.accordionHeading');
+                if ($nextNext.length == 0) {
+                    $this.next('.accordionBody').addClass('last');
+                }
+            });
+
+            $(".accordionHeading").live('click', function () {
+                $that = $(this);
+                $next = $that.next('.accordionBody');
+                $that.removeClass('collapsed');
+                $that.prevUntil('.first').each(function () {
+                    $(this).addClass('collapsed');
+                    $(this).prev('.accordionHeading').addClass('collapsed');
                 });
-        });
-    }());
-    (function () {
-        // control accordion items (TODO)
-    }());
+                $next.nextUntil('.last').each(function () {
+                    $(this).addClass('collapsed');
+                    $(this).next('.accordionBody').addClass('collapsed');
+                });
+            });
+        }());
+    });
 }(jQuery));
