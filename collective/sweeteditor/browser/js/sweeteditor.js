0744267af760e7d12249f347470c273667d7cc58
Davide Moro <davide.moro@gmail,.com>
2015-09-17 15:41:27 +0200

    separate code in different functions

--- collective/sweeteditor/browser/js/sweeteditor.js
+++ collective/sweeteditor/browser/js/sweeteditor.js
@@ -1,11 +1,16 @@
 (function ($){
-    // control expandable items
-    $(".collapsedHeading").live('click',function (){
-        var $heading = $(this);
-        $heading
-            .next()
-            .slideToggle(200, function (){
-                $heading.toggleClass('collapsed');
-            });
-    });
+    (function () {
+        // control expandable items
+        $(".collapsedHeading").live('click',function (){
+            var $heading = $(this);
+            $heading
+                .next()
+                .slideToggle(200, function (){
+                    $heading.toggleClass('collapsed');
+                });
+        });
+    }());
+    (function () {
+        // control accordion items (TODO)
+    }());
 }(jQuery));
