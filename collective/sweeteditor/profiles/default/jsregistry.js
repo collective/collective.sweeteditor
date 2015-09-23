4d89c46a391f138287b74275a6dc9a85dffaf2d8
Davide Moro <davide.moro@gmail,.com>
2015-09-23 12:06:15 +0200

    instal handlebars for markup generation

---
+++ collective/sweeteditor/profiles/default/jsregistry.js
@@ -0,0 +1,9 @@
+<?xml version="1.0"?>
+<object name="portal_javascripts">
+
+ <javascript
+    id="++resource++collective.sweeteditor.js/handlebars-v4.0.2.js"
+    cacheable="True" compression="safe" cookable="True"
+    enabled="True" expression=""  inline="False"/>
+
+</object>
