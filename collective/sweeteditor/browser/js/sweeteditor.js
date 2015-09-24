bd7d7a7d35016b2a69a08c99dbbf8eeff8e3d5a6
Davide Moro <davide.moro@gmail,.com>
2015-09-24 09:37:26 +0200

    remove old code (previous accordion controller)

--- collective/sweeteditor/browser/js/sweeteditor.js
+++ collective/sweeteditor/browser/js/sweeteditor.js
@@ -11,46 +11,5 @@
                     });
             });
         }());
-        (function () {
-            // control accordion items
-            $(".accordionHeading").each(function () {
-                // init accordions
-                var $this = $(this), $prev, $next;
-                $prev = $this.prev('.accordionBody');
-                $next = $this.next('.accordionBody');
-
-                if ($prev.length === 0) {
-                    // this is the first accordion header
-                    $this.removeClass('collapsed');
-                    $this.addClass('first');
-                } else {
-                    $this.addClass('collapsed');
-                    $next.addClass('collapsed');
-                }
-
-                $nextNext = $next.next('.accordionHeading');
-                if ($nextNext.length == 0) {
-                    $this.next('.accordionBody').addClass('last');
-                }
-            });
-
-            $(".accordionHeading").live('click', function () {
-                $that = $(this);
-                $next = $that.next('.accordionBody');
-                $that.removeClass('collapsed');
-                if (! $that.hasClass('first')) {
-                    $that.prevUntil('.first').each(function () {
-                        $(this).addClass('collapsed');
-                        $(this).prev('.accordionHeading').addClass('collapsed');
-                    });
-                }
-                if (! $next.hasClass('last')) {
-                    $next.nextUntil('.last').each(function () {
-                        $(this).addClass('collapsed');
-                        $(this).next('.accordionBody').addClass('collapsed');
-                    });
-                }
-            });
-        }());
     });
 }(jQuery));
