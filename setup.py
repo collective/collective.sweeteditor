e59954cce5525961db6a8f526b162808d1a7bf1d
Davide Moro <davide.moro@gmail,.com>
2015-09-28 17:28:26 +0200

    convert .txt to .rst and update setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,38 @@
 from setuptools import setup, find_packages
-import os
 
 version = '0.1'
 
 setup(name='collective.sweeteditor',
       version=version,
       description="TinyMCE style hooks for accordion and tabs",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" +
+      open("CHANGES.rst").read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+      ],
       keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      author='Davide Moro',
+      author_email='davide.moro@gmail.com',
+      url='https://github.com/davidemoro/collective.sweeteditor',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
           # -*- Extra requirements: -*-
           'collective.monkeypatcher',
       ],
       extras_require={
           'bootstrap': ['plone.app.jquery>=1.9.1'],
-          },
+      },
       entry_points="""
       # -*- Entry points: -*-
 
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
