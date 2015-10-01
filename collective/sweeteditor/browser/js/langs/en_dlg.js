c939a339f490857d2e84d50520cf2afafbf73268
Davide Moro <davide.moro@gmail,.com>
2015-10-01 15:07:36 +0200

    change string

--- collective/sweeteditor/browser/js/langs/en_dlg.js
+++ collective/sweeteditor/browser/js/langs/en_dlg.js
@@ -1,4 +1,4 @@
-tinyMCE.addI18n('en.accordion_dlg', {
+tinyMCE.addI18n("en.accordion_dlg", {
     title : "Accordion",
     legend : "Crea nuovo accordion",
     length_label : "Lunghezza accordion",
