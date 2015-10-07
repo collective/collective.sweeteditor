b2a9745d042bcece303eab56f5031e0c83e4557d
Jian Aijun <jianaijun@gmail.com>
2015-10-07 21:56:23 +0800

    Added Chinese Simplified translation

---
+++ collective/sweeteditor/browser/js/accordion/langs/zh-cn_dlg.js
@@ -0,0 +1,8 @@
+tinyMCE.addI18n("zh-cn.accordion_dlg", {
+    title : "折叠组",
+    legend : "创建新折叠组",
+    length_label : "折叠组长度",
+    length_help : "您想要创建的折叠项数量",
+    collapsable_label : "可折叠",
+    collapsable_help : "不选中: 一次可以展开一个或零折叠项。选中: 一次可以展开多个折叠项。"
+});
