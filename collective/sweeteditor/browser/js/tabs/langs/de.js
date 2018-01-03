a89c7988f05a64cae2ea0221fec21b22c24ef79f
Matthias Barde <matthiasbarde@gmx.de>
2018-01-03 09:49:21 +0100

    add german language files

---
+++ collective/sweeteditor/browser/js/tabs/langs/de.js
@@ -0,0 +1,8 @@
+tinyMCE.addI18n('de.tabs', {
+    group : 'Tabs',
+    desc : 'Tab-Block erstellen',
+    deletedesc : 'Gesamten Tab-Block löschen',
+    itemdeletedesc : 'Tab löschen',
+    iteminsertafterdesc : 'Tab rechts einfügen',
+    iteminsertbeforedesc : 'Tab links einfügen'
+});
